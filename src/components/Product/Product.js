8eaf9db73dd0280a176e1ce8b62c9337eaba07d7
Patryk Barański <pbaranski740@gmail.com>
2023-11-24 10:22:05 +0100

    Wrap price in useMemo

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,23 @@
 import styles from "./Product.module.scss";
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import ProductImage from "./ProductImage";
 import ProductForm from "./ProductForm/ProductForm";
 const Product = ({ id, name, title, colors, sizes, basePrice }) => {
   const [currentSize, setCurrentSize] = useState(sizes[0]);
   const [currentColor, setCurrentColor] = useState(colors[0]);
 
-  const getPriec = () => basePrice + currentSize.additionalPrice;
+  const price = useMemo(
+    () => basePrice + currentSize.additionalPrice,
+    [basePrice, currentSize.additionalPrice]
+  );
 
   const submitHandler = (e) => {
     e.preventDefault();
     console.log("Name:", title);
     console.log("Color:", currentColor);
     console.log("Size:", currentSize.name);
-    console.log("Price", getPriec());
+    console.log("Price", price);
   };
 
   return (
@@ -23,7 +26,7 @@ const Product = ({ id, name, title, colors, sizes, basePrice }) => {
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
-          <span className={styles.price}>Price: {getPriec()}$</span>
+          <span className={styles.price}>Price: {price}$</span>
         </header>
         <ProductForm
           submitHandler={submitHandler}
